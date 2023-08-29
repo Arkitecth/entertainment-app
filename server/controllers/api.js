3236799a254f7b65760bee5339f492f96606bf8e
Trent_T <trenttuedon2000@gmail.com>
2023-08-29 18:27:13 -0400

    Details API Route

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,4 +1,5 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'; 
+
 
 export const trending = async (req, res) => {
     const url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
@@ -8,8 +9,8 @@ export const trending = async (req, res) => {
         accept: 'application/json',
         Authorization: process.env.MOVIE_API
     }
- };
-  
+ }
+    let detailResults = {}; 
     try {
         const response = await fetch(url, options);
         if(!response.ok) {
@@ -17,66 +18,72 @@ export const trending = async (req, res) => {
             throw new Error(message);
         } 
         const trending = await response.json(); 
-        res.status(200).json({trending, isBookmarked:false},); 
+
+        let itemsProcessed = 0 
+        trending.results.forEach((element) => {
+            getTrendingDetails(element).then((data) => {
+                itemsProcessed += 1; 
+                detailResults[data.id] = data;   
+                if(itemsProcessed === trending.results.length) {
+                    res.status(200).json({trending, isBookmarked:false, detailResults}); 
+                }
+            })
+        })
+
     }
     catch(error) {
         console.log(error);
-        res.status(200).json({error:error}); 
+        res.status(500).json({error:error}); 
     }
   
 }
 
 
-export const movieDetails = async (req, res) => {
-    const url = `https://api.themoviedb.org/3/movie/${req.params.id}?append_to_response=release_dates&language=en-US`;
+
+async function callApi(url) {
     const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: process.env.MOVIE_API
-    }
- };
-  
-    try {
-        const response = await fetch(url, options);
-        if(!response.ok) {
-            const message = `An error occured: ${response.status}`
-            throw new Error(message);
-        } 
-        const details = await response.json(); 
-        res.status(200).json(details); 
-    }
-    catch(error) {
-        console.log(error);
-        res.status(200).json({error:error}); 
-    }
-  
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: process.env.MOVIE_API
+        }
+     };
+    
+        try {
+            const response = await fetch(url, options);
+            if(!response.ok) {
+                const message = `An error occured: ${response.status}`
+                throw new Error(message);
+            } 
+            const results = await response.json(); 
+            return results; 
+        }
+        catch(error) {
+            console.log(error);
+        }
 }
 
-export const seriesDetails = async (req, res) => {
-    const url = `https://api.themoviedb.org/3/movie/${req.params.id}?append_to_response=release_date&language=en-US`;
-    const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: process.env.MOVIE_API
+async function getTrendingDetails(element) {
+    let trendingUrl = ''
+    if(element.media_type === "movie") {
+       trendingUrl = `https://api.themoviedb.org/3/${element.media_type}/${element.id}?append_to_response=release_dates&language=en-US`;
+    } else {
+        trendingUrl = `https://api.themoviedb.org/3/${element.media_type}/${element.id}?append_to_response=content_ratings&language=en-US`;
     }
- };
-  
-    try {
-        const response = await fetch(url, options);
-        if(!response.ok) {
-            const message = `An error occured: ${response.status}`
-            throw new Error(message);
-        } 
-        const details = await response.json(); 
-        res.status(200).json(details); 
-    }
-    catch(error) {
-        console.log(error);
-        res.status(200).json({error:error}); 
-    }
-  
+    return callApi(trendingUrl);
 }
+
+export const details = async (req, res) => {
+    let url = '';
+    if(req.params.name === "movie")
+    {
+        url = `https://api.themoviedb.org/3/${req.params.name}/${req.params.id}?append_to_response=release_dates&language=en-US`;
+    } else {
+        url = `https://api.themoviedb.org/3/${req.params.name}/${req.params.id}?append_to_response=content_ratings&language=en-US`;
+    }
+    return  res.status(200).json(callApi(url));  
+}
+  
+
 
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express"
 import {login, register} from "../controllers/auth.js"
-import { trending, movieDetails, seriesDetails } from "../controllers/api.js";
+import { trending, details} from "../controllers/api.js";
 
 const router = express.Router(); 
 
@@ -11,9 +11,8 @@ router.post("/login", login)
 
 router.get('/trending', trending)
 
-router.get('/details/movie/:id', movieDetails)
+router.get('/details/:name/:id', details)
 
-router.get('/details/series/:id', seriesDetails)
 
 
 export default router;
